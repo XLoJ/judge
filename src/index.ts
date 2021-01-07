d6578760bc5ddb6a76781566339c7780f72fe29b
XLor <yjl9903@vip.qq.com>
2021-01-07 23:48:45 +0800

    add: logger, uuid

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import fastify from 'fastify';
+import hyperid from 'hyperid';
 
 async function bootstrap() {
-  const server = fastify({ logger: true });
+  const server = fastify({
+    bodyLimit: 16 * 1024 * 1024, // 16 MB
+    logger: {
+      prettyPrint: true,
+    },
+    genReqId(req) {
+      const instance = hyperid({ urlSafe: true });
+      return instance.uuid;
+    },
+  });
 
   await server.listen(3000, '127.0.0.1');
 }
